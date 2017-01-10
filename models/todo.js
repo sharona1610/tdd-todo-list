e93ed2a5929093714433461d3577b916e95b66f7
sharona1610 <sharona1610@gmail.com>
2017-01-10 15:54:59 +0800

    changes

--- models/todo.js
+++ models/todo.js
@@ -1,27 +1,75 @@
 const uuidGenerator = require('uuid/v4')
+var fs = require("fs");
 
 let todos = []
-
+var idVal=1
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
-  // uuid usage exmaple
-  // let uuid = uuidGenerator()
+  var uuid = uuidGenerator()
+  if(params.hasOwnProperty('name')){
+    if(params.name.length>5){
+      var newTodo = {
+        id_num: idVal,
+        name: params.name,
+        description: params.description || 'Just some default value',
+        completed: params.completed || false,
+        _id: uuid
+      }
+      todos.push(newTodo)
+      idVal++
+    }
+  }
+  fs.writeFile( "saved.json", JSON.stringify(todos), "utf8", yourCallback );
+  return todos
 }
+// create({name: 'wsdgs', description:'fgsr'})
+//console.log(todos);
 
-// READ (list & show)
+//READ (list & show)
 function list () {
   return todos
 }
-function show (id) {}
+
+function show (id) {
+  for(var key in todos){
+    if(todos[key].id_num===id){
+      return todos[key]
+    }
+  }
+  return null
+}
 
 // UPDATE - params should be an object with KVPs for the fields to update
-function update (id, updatedParams) {}
+function update (id, updatedParams) {
+  if(updatedParams.name!='' && updatedParams.name.length>5){
+  for(var key in todos){
+    if(todos[key].id_num===id){
+      todos[key].name = updatedParams.name
+      todos[key].description= updatedParams.description
+      todos[key].completed= updatedParams.completed
+      return true
+    }
+  }
+}
+  return false
+}
 
 // DESTROY (destroy & destroyAll)
-function destroy (id) {}
+function destroy (id) {
+  for(var key in todos){
+    if(todos[key].id_num===id){
+      todos.splice(key,1)
+      return true
+    }
+  }
+  return false
+}
 function destroyAll () {
+  todos=[]
   return true
 }
+
+
 
 module.exports = {
   create: create,
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -2,6 +2,26 @@ const assert = require('assert')
 const todos = require('../models/todo.js')
 
 // Example Test - we expect that when we run destroyAll, it should return true to let us know it was successful
-assert.strictEqual( todos.destroyAll() , true, 'DestroyAll should return true, to indicate success')
-// We also expect the list should now be empty
-assert.strictEqual( todos.list().length , 0, 'List should be empty after deleting all TODOs')
+// assert.strictEqual( todos.destroyAll() , true, 'DestroyAll should return true, to indicate success')
+// // We also expect the list should now be empty
+// assert.strictEqual( todos.list().length , 0, 'List should be empty after deleting all TODOs')
+// assert.strictEqual( todos.list().length , 0, 'List should be empty after deleting all TODOs')
+assert.strictEqual( todos.create({name:'task number 1', description:'blah' , completed: true}).length, 1, 'New todo not added')
+assert.strictEqual( todos.create({name:'task number 2', description:'blah' , completed: true})[0].hasOwnProperty('_id'), true, 'UUID not added')
+assert.strictEqual( todos.create({name:'task number 3'}).length, 3, 'New todo not created with just name and default values for other')
+assert.strictEqual( todos.create({description:'blah'}).length, 3, 'New todo created even with name not given')
+assert.strictEqual( todos.create({name: '1234', description:'blah'}).length, 3, 'New todo created even with name not meeting required length')
+
+assert.strictEqual(todos.list().length, 3, 'Function does not list')
+
+assert.strictEqual(todos.show(2).id_num, 2, 'Function does not return the correct task')
+assert.strictEqual(todos.show(6), null, 'Function does not return null')
+
+assert(todos.update(2,{name: 'task number changed', description: 'blah', completed: false}),'Updation not successful')
+assert(!todos.update(2,{name: 'task', description: 'blah', completed: false}),'Updation not successful')
+assert(!todos.update(2,{name: '', description: 'blah', completed: false}),'Updation not successful')
+
+assert(todos.destroy(2),'The task has not been deleted')
+assert(!todos.destroy(7),'The task id does not exist, but the function deleted some id')
+
+assert(todos.destroyAll(),'The todos list has not been emptied')
